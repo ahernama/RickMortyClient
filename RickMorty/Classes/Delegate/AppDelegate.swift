f1e0adf848d8374ebfe24880b8952f0bad29d5d8
Alejandro Hernández Matías <alex@4andgo.com>
2018-10-30 13:20:45 +0100

    Firebase configuration

--- RickMorty/Classes/Delegate/AppDelegate.swift
+++ RickMorty/Classes/Delegate/AppDelegate.swift
@@ -8,14 +8,22 @@
 
 import UIKit
 
+import Firebase
+import RealmSwift
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        //Firebase Init
+        FirebaseApp.configure()
+        
+        //Realm Check configuration
+        self.checkDatabase()
+        
         return true
     }
 
@@ -40,7 +48,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-
+    
+    //Check Real Database configuration methods
+    func checkDatabase(){
+        let config = Realm.Configuration(
+            schemaVersion: UInt64(DevDefines.datbaseVersion),
+            migrationBlock: { migration, oldSchemaVersion in
+                if oldSchemaVersion < UInt64(DevDefines.datbaseVersion) {
+                }
+        })
+        Realm.Configuration.defaultConfiguration = config
+    }
 }
 
