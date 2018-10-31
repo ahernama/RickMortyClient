947db913e945ffed911993b22932d638564982dc
Alejandro Hernández Matías <alex@4andgo.com>
2018-10-31 19:55:00 +0100

    Adding main files in episode detail feature

---
+++ RickMorty/Classes/Modules/Detail/Managers/CharactersManager.swift
@@ -0,0 +1,13 @@
+//
+//  CharactersManager.swift
+//  RickMorty
+//
+//  Created by Alejandro Hernández Matías on 31/10/18.
+//  Copyright © 2018 Alejandro Hernández Matías. All rights reserved.
+//
+
+import UIKit
+
+class CharactersManager: NSObject {
+
+}
---
+++ RickMorty/Classes/Modules/Detail/Presentation/UI/ViewControllers/CharactersViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CharactersViewController.swift
+//  RickMorty
+//
+//  Created by Alejandro Hernández Matías on 31/10/18.
+//  Copyright © 2018 Alejandro Hernández Matías. All rights reserved.
+//
+
+import UIKit
+
+class CharactersViewController: RickMortyViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
